d5c994d9d9ee1663afa651ba0d711623cd27942c
Manishvyas7 <manishvyas210@gmail.com>
2026-01-18 10:17:09 +0530

    Binary Search Implementation

---
+++ SearchingInArrays/Intermediate/BinarySearchImplementation.java
@@ -0,0 +1,30 @@
+package Intermediate;
+
+public class BinarySearchImplementation {
+    // Binary Search Method
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12};
+        int target = 8;
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
